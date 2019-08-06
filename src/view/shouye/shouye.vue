6978d56723f34a6cfd3a3b7eea6c773eb850da8e
always <1593490491@qq.com>
2019-08-06 10:33:02 +0800

    Initial commit

---
+++ src/view/shouye/shouye.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="app">
+
+
+
+
+        hello word
+
+
+
+    </div>
+</template>
+
+<script>
+
+    import axios from 'axios'
+
+    export default {
+        name: 'App',
+        data() {
+
+            var self = this
+            return {
+
+
+            }
+
+
+        },
+
+    }
+</script>
+
+<style>
+
+</style>
